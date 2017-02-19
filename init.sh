3288d8e90dd01050a1caf322ec42cee3c33fe393
hloeffler <hloeffler@users.noreply.github.com>
2017-02-19 18:47:52 +0100

    add more

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo apt-get install  --no-install-recommends $(cat apt.list| xargs)
+#Useless Use of Cat Award - http://porkmail.org/era/unix/award.html#cat
+sudo apt-get install  --no-install-recommends $(cat apt.list| grep -v -e '^#' -e '^$' | sort | uniq | xargs)
