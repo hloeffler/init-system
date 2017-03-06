0f24b0727af0b527b15e5de9e01e478b470d1be5
Hauke Löffler <hloeffler@users.noreply.github.com>
2017-03-06 17:19:06 +0100

    1 file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # init-system
 
 current base: Ubuntu 16.04
+
+
+```
+wget https://raw.githubusercontent.com/hloeffler/init-system/master/install.sh
+chmod +x install.sh
+```
--- init.sh
+++ init.sh
@@ -4,6 +4,14 @@
 
 PKG_LIST=$(cat apt.list | grep -v -e '^#' -e '^$' apt.list | sort | uniq | xargs)
 
-sudo apt-get install --no-install-recommends ${PKG_LIST}
+TARGET_FILE="install.sh"
+
+echo "#!/bin/sh" > ${TARGET_FILE}
+
+echo "sudo apt-get install --no-install-recommends \\" >> ${TARGET_FILE}
+
+for p in ${PKG_LIST}; do
+  echo "${p} \\" >> ${TARGET_FILE};
+done
 
 
