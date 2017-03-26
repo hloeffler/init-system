bad5f5ef05d90c2cf826873cb9bdc051c7b1f13e
Hauke Löffler <hloeffler@users.noreply.github.com>
2017-03-26 09:31:55 +0200

    update support

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -2,7 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mkdir -p "$HOME/.dotfiles/"
-cd "$HOME/.dotfiles/"
-git clone git@github.com:hloeffler/zsh-config.git
+DDIRECTORY="$HOME/.dotfiles/"
+mkdir -p "${DDIRECTORY}"
+cd "${DDIRECTORY}"
+
+DDIRECTORY_ZSH="${DDIRECTORY}/zsh-config"
+
+if [ -d "$DDIRECTORY_ZSH" ]; then
+  cd $DDIRECTORY_ZSH && git pull
+  cd -
+else
+  git clone git@github.com:hloeffler/zsh-config.git
+fi
 stow zsh-config
